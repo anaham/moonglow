dc23906ca4cc12bc53aec2a55959bb3d4259ad9d
anaham <wiredkj@gmail.com>
2018-03-23 09:36:46 +0900

    Update sc_ex01.py

--- sc_ex01.py
+++ sc_ex01.py
@@ -3,3 +3,5 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
 bs0bj = BeautifulSoup(html.read(),"html.parser")
 print(bs0bj.h1)
+
+what is the commit means?
